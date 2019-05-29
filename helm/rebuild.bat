c3cbfc63362bc875fc11be0636970bcc0f814dfa
David Parker <parkerda@uk.ibm.com>
2019-05-29 17:29:56 +0100

    Add oshi to helm chart repo

--- helm/rebuild.bat
+++ helm/rebuild.bat
@@ -1,2 +1,3 @@
 ..\..\iot-python\samples\helm package ../../iot-python/samples/psutil/helm/psutil -d charts
+..\..\iot-python\samples\helm package ../../iot-java/samples/oshi/helm/oshi -d charts
 ..\..\iot-python\samples\helm repo index charts --url https://ibm-watson-iot.github.io/helm/charts/
